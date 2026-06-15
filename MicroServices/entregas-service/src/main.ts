5c6988c17a9811328e7ed7a7debca54d2963f9ec
Milagros Alvarado <milagrosalvarado1248@gmail.com>
2026-06-15 19:43:21 -0300

    "Se cargó el punto de entrada de la aplicación en main.ts y agregue  "types": ["node"] faltante en  tsconfig.json"

---
+++ MicroServices/entregas-service/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        })
+    );
+const config = new DocumentBuilder()
+    .setTitle('Entregas Service')
+    .setDescription('API para gerenciamento de entregas - AguaFress')
+    .setVersion('1.0')
+    .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api/docs', app, document);
+const PORT = process.env.PORT || 3005;
+await app.listen(PORT, () => {
+    console.log(`Entregas Service corriendo en el puerto ${PORT}`);
+}
+}
+bootstrap();
