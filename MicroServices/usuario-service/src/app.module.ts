2ac080463d8065bb0b2c2d9b79e2aa94dfe0bfed
Ladry <burdilesricardo407@gmail.com>
2026-06-27 15:37:51 -0300

    feat(vendedores): add VendedoresModule with stubs and register in AppModule
    - VendedoresModule imports AuthModule + CommonModule
    - Registers VendedoresService, VendedoresController, VendedorProfileController, VendedorGuard
    - Exports VendedoresService for cross-module use
    - Adds VendedoresModule to AppModule imports
    - Stub service + controllers for compilation (full impl in later phases)

--- MicroServices/usuario-service/src/app.module.ts
+++ MicroServices/usuario-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { VendedoresModule } from './vendedores/vendedores.module';
 import { CommonModule } from './common/common.module';
 import jwtConfig from './common/config/env.config';
 
@@ -10,6 +11,7 @@ import jwtConfig from './common/config/env.config';
     ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
     AuthModule,
     UsersModule,
+    VendedoresModule,
     CommonModule,
   ],
 })
---
+++ MicroServices/usuario-service/src/vendedores/vendedores.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { CommonModule } from '../common/common.module';
+import { VendedoresService } from './vendedores.service';
+import { VendedoresController } from './vendedores.controller';
+import { VendedorProfileController } from './vendedor-profile.controller';
+import { VendedorGuard } from './guards/vendedor.guard';
+
+@Module({
+  imports: [AuthModule, CommonModule],
+  controllers: [VendedoresController, VendedorProfileController],
+  providers: [VendedoresService, VendedorGuard],
+  exports: [VendedoresService],
+})
+export class VendedoresModule {}
